aed6f3152416f78243934c94646fc872df1d04ff
dbenfon341 <dbenfon341@g.educaand.es>
2024-12-04 14:44:15 +0100

    typo

--- src/playground.py
+++ src/playground.py
@@ -7,7 +7,7 @@
 
 # TODO
 """
-1 Se crea una partida en x lugar llamada partidas_hundirflota.
+1 Se crea una carpeta en x directorio llamada partidas_hundirflota.
 2 Dentro, subcarpetas con los nombres de las partidas en cuestion y los archivos de configuración default.
 3 Al inicializar el programa, se muestra un menu para seleccionar si quieres crear una partida o continuar una ya empezada (para el caso del 2º player por ej).
 4 Si se selecciona cear partida, se pedira nombre de la partida y se verificará que la partida no existe, en el caso de ya existir, se pedirá si quiere empezar de 0 o continuarla.
