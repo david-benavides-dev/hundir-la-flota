cd1948da09618efa3cc64fd555d79b2af5412ab5
dbenfon341 <dbenfon341@g.educaand.es>
2024-12-04 14:16:47 +0100

    Pensando lógica inicio de juego

---
+++ src/playground.py
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+
+# TODO
+"""
+1 Se crea una partida en x lugar llamada partidas_hundirflota.
+2 Dentro, subcarpetas con los nombres de las partidas en cuestion y los archivos de configuración default.
+3 Al inicializar el programa, se muestra un menu para seleccionar si quieres crear una partida o continuar una ya empezada (para el caso del 2º player por ej).
+4 Si se selecciona cear partida, se pedira nombre de la partida y se verificará que la partida no existe, en el caso de ya existir, se pedirá si quiere empezar de 0 o continuarla.
+5 Caso de no existir, se creará una carpeta con toda la configuración inicial, tanto base como J1 y J2.
+"""
