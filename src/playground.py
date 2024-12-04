e9efb59ca500fd439a97848a2923797874435830
dbenfon341 <dbenfon341@g.educaand.es>
2024-12-04 15:00:04 +0100

    Analisis mas detallado para futuro

--- src/playground.py
+++ src/playground.py
@@ -13,7 +13,7 @@ import json
 
 # 2. Al inicializar el programa, se muestra un menu para seleccionar si quieres crear una partida o continuar una ya empezada (para el caso del 2º player por ej).
 # 3. Si se selecciona cear partida, se pedira nombre de la partida y se verificará que la partida no existe, en el caso de ya existir, se pedirá si quiere empezar de 0 o continuarla.
-# 4. Caso de no existir, se creará una carpeta con toda la configuración inicial, tanto base como J1 y J2.
+#   - Caso de no existir, se creará una carpeta con el nombre de la partida mas toda la configuración inicial, tanto base como J1 y J2.
 
 
 def pausa() -> None:
