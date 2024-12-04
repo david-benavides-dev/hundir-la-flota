e1e83df2d9636a24e846d43d8c5e9f0f3bffeca7
dbenfon341 <dbenfon341@g.educaand.es>
2024-12-04 14:48:35 +0100

    cleanup

--- src/playground.py
+++ src/playground.py
@@ -7,9 +7,15 @@
 
 # TODO
 """
-1 Se crea una carpeta en x directorio llamada partidas_hundirflota.
-2 Dentro, subcarpetas con los nombres de las partidas en cuestion y los archivos de configuración default.
-3 Al inicializar el programa, se muestra un menu para seleccionar si quieres crear una partida o continuar una ya empezada (para el caso del 2º player por ej).
-4 Si se selecciona cear partida, se pedira nombre de la partida y se verificará que la partida no existe, en el caso de ya existir, se pedirá si quiere empezar de 0 o continuarla.
-5 Caso de no existir, se creará una carpeta con toda la configuración inicial, tanto base como J1 y J2.
+1. Se crea una carpeta en x directorio llamada partidas_hundirflota. EJ:
+    C:\Usuarios\User\AppData\Local\partidas_hundirflota
+        - Dentro de la misma se crearán subcarpetas con los nombres de las partidas en cuestion y los archivos de configuración default. EJ:
+            C:\Usuarios\User\AppData\Local\partidas_hundirflota\partida1
+                C:\Usuarios\User\AppData\Local\partidas_hundirflota\partida1\config.json
+                C:\Usuarios\User\AppData\Local\partidas_hundirflota\partida1\j1.json
+                C:\Usuarios\User\AppData\Local\partidas_hundirflota\partida1.j2.json
+
+2. Al inicializar el programa, se muestra un menu para seleccionar si quieres crear una partida o continuar una ya empezada (para el caso del 2º player por ej).
+3. Si se selecciona cear partida, se pedira nombre de la partida y se verificará que la partida no existe, en el caso de ya existir, se pedirá si quiere empezar de 0 o continuarla.
+4. Caso de no existir, se creará una carpeta con toda la configuración inicial, tanto base como J1 y J2.
 """
